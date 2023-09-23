c824650f6f138aa430bc903d92367f982148b250
Aprkellas <alex.kellas.relton@gmail.com>
2023-09-23 13:38:50 +0100

    MainMenuViewModel setup

--- ViewModels/MainMenuViewModel.cs
+++ ViewModels/MainMenuViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using C___hess.Services;
+using YourNamespace.Commands;
 
 namespace C___hess.ViewModels
 {
@@ -10,7 +15,27 @@ namespace C___hess.ViewModels
     {
         public MainMenuViewModel() 
         {
+            play = new RelayCommand(Play);
+            instructions = new RelayCommand(ShowInstructions);
+            quit = new RelayCommand(QuitGame);
+        }
+
+        private void Play()
+        {
 
         }
+        private void ShowInstructions()
+        {
+
+        }
+
+        private void QuitGame()
+        {
+
+        }
+
+        public ICommand play { get; }
+        public ICommand instructions { get; }
+        public ICommand quit { get; }
     }
 }
