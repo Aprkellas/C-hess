5e5649a47a39ac418419c1311831368836dbf1b8
Aprkellas <alex.kellas.relton@gmail.com>
2023-09-23 18:31:31 +0100

    Added king and queen class

---
+++ Models/King.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C___hess.Models
+{
+    class King
+    {
+        public King() 
+        {
+            move_dist = 1;
+            diagonal = false;
+            horizontal = true;
+
+
+        }
+        private int move_dist;
+        private bool diagonal;
+        private bool horizontal;
+    }
+}
---
+++ Models/Queen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C___hess.Models
+{
+    class Queen 
+    {
+        public Queen() 
+        {
+            move_dist = 20;
+            diagonal = true;
+            horizontal = true;
+        }
+        private int move_dist;
+        private bool diagonal;
+        private bool horizontal;
+    }
+   
+}
