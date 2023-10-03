b1413b8f9c74368958178208c9c622b443790bf5
Aprkellas <alex.kellas.relton@gmail.com>
2023-10-03 21:12:49 +0100

    pieces array made, along with switch statement structure

--- ViewModels/GameViewModel.cs
+++ ViewModels/GameViewModel.cs
@@ -16,6 +16,34 @@ namespace C___hess.ViewModels
         }
         private void LoadComponents()
         {
+            int[,] pieces = new int[8, 8]
+            {
+                {4, 2, 3, 5, 6, 3, 2, 4},
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {4, 2, 3, 5, 6, 3, 2, 4}
+            };
+
+            int pieceNumb = 0;
+
+            foreach (int piece in pieces)
+            {
+                pieceNumb++;
+                if (pieceNumb < 32) { 
+                    switch (piece)
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                            break;
+                    }
+                }
+            }
         }
+
     }
 }
