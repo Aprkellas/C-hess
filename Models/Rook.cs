d317e1df1b06751626274dce87f659ea9a782450
Aprkellas <alex.kellas.relton@gmail.com>
2023-10-06 14:27:48 +0100

    base rook class added

---
+++ Models/Rook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C___hess.Models
+{
+    class Rook : Piece
+    {
+        public Rook(bool isWhite, int x, int y ) : base(isWhite, x,y)
+        {
+            moveDist = 20;
+            isDiagonal = false;
+            isHorizontal = true;
+        }
+    }
+}
