2a55e16969a1be880ab9377c3e53c0e58aebe5b9
Aprkellas <alex.kellas.relton@gmail.com>
2023-10-06 14:31:30 +0100

    bishop class added

---
+++ Models/Bishop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C___hess.Models
+{
+    class Bishop : Piece
+    {
+        public Bishop(bool isWhite, int x int y) : base(isWhite, x, y)
+        {
+            moveDist = 20;
+            isDiagonal = true;
+            isHorizontal = false;
+        }
+    }
+}
